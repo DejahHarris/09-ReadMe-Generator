f876151bcd3c5d2b89208f740ee656c3e9e1e5d3
Dejah Harris <dejahharris@Dejahs-MBP.attlocal.net>
2022-12-08 19:47:26 -0500

    added packages to index

--- index.js
+++ index.js
@@ -1,4 +1,64 @@
+const inquirer = require("inquirer");
+const path = require('path');
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown.js')
 // TODO: Include packages needed for this application
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project here:'
+
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How is your software installed?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How is your software used?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How is your software used?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: "What kind of license does your project have?",
+        choices: ['MIT', 'Apache', 'GPL']
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'Where can people contribute to this project?'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How can a user test your software?'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub user name?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+
+];
 
 // TODO: Create an array of questions for user input
 const questions = [];
